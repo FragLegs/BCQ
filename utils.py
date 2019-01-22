830070e066ea5fb319b9b4d7b0167af6b710a2ef
FragLegs <miel.shayne@gmail.com>
2019-01-22 11:03:33 -0500

    Fix linting errors in util.py

--- utils.py
+++ utils.py
@@ -1,37 +1,40 @@
 import numpy as np
 
-# Code based on: 
+# Code based on:
 # https://github.com/openai/baselines/blob/master/baselines/deepq/replay_buffer.py
+
 
 # Simple replay buffer
 class ReplayBuffer(object):
-	def __init__(self):
-		self.storage = []
+    def __init__(self):
+        self.storage = []
 
-	# Expects tuples of (state, next_state, action, reward, done)
-	def add(self, data):
-		self.storage.append(data)
+    # Expects tuples of (state, next_state, action, reward, done)
+    def add(self, data):
+        self.storage.append(data)
 
-	def sample(self, batch_size):
-		ind = np.random.randint(0, len(self.storage), size=batch_size)
-		state, next_state, action, reward, done = [], [], [], [], []
+    def sample(self, batch_size):
+        ind = np.random.randint(0, len(self.storage), size=batch_size)
+        state, next_state, action, reward, done = [], [], [], [], []
 
-		for i in ind: 
-			s, s2, a, r, d = self.storage[i]
-			state.append(np.array(s, copy=False))
-			next_state.append(np.array(s2, copy=False))
-			action.append(np.array(a, copy=False))
-			reward.append(np.array(r, copy=False))
-			done.append(np.array(d, copy=False))
+        for i in ind:
+            s, s2, a, r, d = self.storage[i]
+            state.append(np.array(s, copy=False))
+            next_state.append(np.array(s2, copy=False))
+            action.append(np.array(a, copy=False))
+            reward.append(np.array(r, copy=False))
+            done.append(np.array(d, copy=False))
 
-		return (np.array(state), 
-			np.array(next_state), 
-			np.array(action), 
-			np.array(reward).reshape(-1, 1), 
-			np.array(done).reshape(-1, 1))
+        return (
+            np.array(state),
+            np.array(next_state),
+            np.array(action),
+            np.array(reward).reshape(-1, 1),
+            np.array(done).reshape(-1, 1)
+        )
 
-	def save(self, filename):
-		np.save("./buffers/"+filename+".npy", self.storage)
+    def save(self, filename):
+        np.save('./buffers/{}.npy'.format(filename), self.storage)
 
-	def load(self, filename):
-		self.storage = np.load("./buffers/"+filename+".npy")
+    def load(self, filename):
+        self.storage = np.load('./buffers/{}.npy'.format(filename))
